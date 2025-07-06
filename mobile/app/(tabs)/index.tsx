d66970e23480774463020bdfd39e272496c4b880
Eliana Dheys B. Ojeda <164065005+liandh@users.noreply.github.com>
2025-07-06 16:38:44 +0800

    user to db added

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import SignOutButton from "@/components/SignOutButton";
+import { useUserSync } from "@/hooks/useUserSync";
 
 const HomeScreen = () => {
+  useUserSync();
   return (
     <SafeAreaView className="flex-1">
       <Text>HomeScreen</Text>
