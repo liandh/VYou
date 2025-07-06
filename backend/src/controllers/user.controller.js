fe90d2a71066c490163c30433138e4b5180b8406
Eliana Dheys B. Ojeda <164065005+liandh@users.noreply.github.com>
2025-07-06 16:19:50 +0800

    fixed file name

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -37,7 +37,7 @@ export const syncUser = asyncHandler(async (req, res) => {
     email: clerkUser.emailAddresses[0].emailAddress,
     firstName: clerkUser.firstName || "",
     lastName: clerkUser.lastName || "",
-    username: clerkUser.emailAddresses[0].emailAddress.split("@")[0],
+    userName: clerkUser.emailAddresses[0].emailAddress.split("@")[0],
     profilePicture: clerkUser.imageUrl || "",
   };
 
